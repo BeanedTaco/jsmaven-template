bd399459892e1ecfdfd8a18614748b4a20b127be
SaturdayNightDead <git@bean.codes>
2020-06-12 14:50:18 -0700

    Update example.js

--- com.example.example/example.js
+++ com.example.example/example.js
@@ -5,10 +5,11 @@ const yaml = require('js-yaml');
 const db = require("quick.db")
 const package = yaml.safeLoad(fs.readFileSync(config.friendlyblobs.giphy.jsmaven)) // This refers to your jsmaven.yaml file.
 exports.run = (client, message, args, level) => {
+    client.mongoprefix.findOne({ guild: message.guild.id }, async (err, prefix) => { // Unicord has recently switched to mongoDB for prefixes.
     const settings = message.settings = client.getSettings(message.guild);
     if (db.get(`${message.guild.id}.nameOfPackageNOTFRIENDLYNAME`) == false) return;
         if (db.get(`${message.guild.id}.nameOfPackageNOTFRIENDLYNAME`) == null) return;
-
+    });
 };
 
 exports.conf = {
